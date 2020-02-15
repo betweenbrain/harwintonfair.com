2b5352f2f486f526c4aae6110fd84fd8a6daba57
Matt Thomas <mthomas@guggenheim.org>
2020-02-14 19:38:12 -0500

    Adds example of loading template part based on request (pseudo code)

--- wp-content/themes/harwintonfair/index.php
+++ wp-content/themes/harwintonfair/index.php
@@ -5,11 +5,30 @@
 
 get_header();
 
-if (have_posts()) : ?>
+/*
+// Load template part based on request
+
+global $wp;
+$current_url = add_query_arg( array(), $wp->request );
+
+// echo '<pre>' . print_r($wp->request, true) . '</pre>';
+// Add logic here:
+
+$load = locate_template( 'archive-activity.php', true );
+if ( $load ) {
+	// just exit if template was found and loaded
+	exit();
+}
+*/
+
+if ( have_posts() ) : ?>
 	<main class="wrapper" role="main">
-		<?php while (have_posts()) : the_post(); ?>
-		<?php $type = get_post_type(); ?>
-			<?php get_template_part('parts/' . $type); ?>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<?php $type = get_post_type(); ?>
+			<?php get_template_part( 'parts/' . $type ); ?>
 		<?php endwhile; ?>
 	</main>
 <?php endif; ?>
